97415d6a9cb6b7e752a13997fb814cf8ff18ed37
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-08-04 20:55:43 -0300

    Reparando tests.

--- tests/integration/components/enjambre-componentes/logo/component-test.js
+++ tests/integration/components/enjambre-componentes/logo/component-test.js
@@ -1,25 +1,15 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+import { moduleForComponent, test } from "ember-qunit";
+import hbs from "htmlbars-inline-precompile";
 
-moduleForComponent('enjambre-componentes/logo', 'Integration | Component | enjambre componentes/logo', {
-  integration: true
-});
+moduleForComponent(
+  "enjambre-componentes/logo",
+  "Integration | Component | enjambre componentes/logo",
+  {
+    integration: true
+  }
+);
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test("it renders", function(assert) {
   this.render(hbs`{{enjambre-componentes/logo}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#enjambre-componentes/logo}}
-      template block text
-    {{/enjambre-componentes/logo}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.ok(this.$().text());
 });
